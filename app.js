29ae9f54b63cd4652785a0383a56ea9ad74cc181
rupert-ong <rupert.ong@gmail.com>
2018-02-17 13:54:07 -0500

    Add express and morgan dependencies. Add 404 and error handler.

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+
+// Routes
+
+// 404 Errors
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error Handler
+app.use((err, req, res, next) => {
+  const error = app.get('env') === 'development' ? err : {};
+  const status = err.status || 500;
+
+  // Server output
+  console.error(err);
+
+  // Client output
+  res.status(status).json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
+// Start the server
+const port = app.get('port') || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});

15f39a775c2d6d7149fd71543e1475e5f90f8d39
rupert-ong <rupert.ong@gmail.com>
2018-02-18 05:48:51 -0500

    Change Promises to Async/Await

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,19 @@
 const User = require('../models/user');
 
-exports.get_users = (req, res, next) => {
-  User.find({})
-    .exec()
-    .then(users => {
-      res.status(200).json(users);
-    }).catch(err => {
-      next(err);
-    });
+exports.get_users = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.create_user = (req, res, next) => {
-  User.create(req.body)
-    .then(user => {
-      res.status(201).json(user);
-    }).catch(err => {
-      next(err);
-    });
+exports.create_user = async (req, res, next) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
